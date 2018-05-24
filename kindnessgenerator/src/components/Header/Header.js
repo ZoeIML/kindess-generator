4998bc33569a46b4567908cd4e40b917446537aa
zoeIML <zoe.lisk@gmail.com>
2018-05-25 11:32:01 +1200

    Header component renders in App

---
+++ kindnessgenerator/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text h1>Random Acts of Kindness Generator</Text>
+            </View>
+        )
+    }
+}
+
+export default Header
