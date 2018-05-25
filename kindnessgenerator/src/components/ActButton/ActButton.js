d572f989539d6da98aaaac23a5eff8c07e33593e
zoeIML <zoe.lisk@gmail.com>
2018-05-25 16:31:01 +1200

    add test onPress function to ActButton

--- kindnessgenerator/src/components/ActButton/ActButton.js
+++ kindnessgenerator/src/components/ActButton/ActButton.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import { View, Button } from 'react-native'
 
+// props is getAct function
+
+// import styles from './styles'
+
 export default class ActButton extends React.Component {
+    constructor (props) {
+        super(props)
+        this.handlePress = this.handlePress.bind(this)
+    }
+
+    handlePress () {
+        return <View>Test It!</View>
+    }
+
     render () {
         return (
             <View>
