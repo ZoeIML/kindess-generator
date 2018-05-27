b971c42c027e1719514abd15855bd055c67db8b4
zoeIML <zoe.lisk@gmail.com>
2018-05-28 10:21:27 +1200

    add color to Intro

--- kindnessgenerator/src/components/Intro/Intro.js
+++ kindnessgenerator/src/components/Intro/Intro.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
-// import styles from './styles'
+import styles from './styles'
 
 export default class Intro extends React.Component {
     render() {
         return (
             <View>
-                <Text> Brighten someone's day with a Random Act of Kindness. Click ACT below for ideas! </Text>
+                <Text style={styles.text}> Brighten someone's day with a Random Act of Kindness</Text>
             </View>
         )
     }
--- kindnessgenerator/src/components/Intro/styles.js
+++ kindnessgenerator/src/components/Intro/styles.js
@@ -1,3 +1,11 @@
 'use strict'
 
 import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    text: {
+        color: 'darkgreen',
+        fontSize: 20,
+        textAlign: 'center'
+    }
+})
