2bd69891702dbd71bcc6cd1599263a5623752ba5
zoeIML <zoe.lisk@gmail.com>
2018-05-28 10:36:42 +1200

    add color to Act component

--- kindnessgenerator/src/components/Act/Act.js
+++ kindnessgenerator/src/components/Act/Act.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 
+import styles from './styles'
+
 export default class Act extends React.Component {
     render () {
         return (
             <View>
-                <Text>{this.props.act}</Text>
+                <Text style={styles.text}>{this.props.act}</Text>
             </View>
         )
     }
